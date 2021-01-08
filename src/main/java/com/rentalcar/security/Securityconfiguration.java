baf4be2ae1c273aafe4d0035c29d123c97251d88
RossiFire <fratsavio.dr@gmail.com>
2021-01-08 15:17:42 +0100

    update

--- src/main/java/com/rentalcar/security/Securityconfiguration.java
+++ src/main/java/com/rentalcar/security/Securityconfiguration.java
@@ -21,57 +21,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class Securityconfiguration extends WebSecurityConfigurerAdapter{
-
-	
-//	@Autowired
-//	UserDetailsService userDetailsService;
-//	
-//	@Bean
-//	public BCryptPasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-//		auth.userDetailsService(userDetailsService);
-//	}
-//	
-//	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception{
-//		http.authorizeRequests()
-//		.antMatchers("/").permitAll()
-//		.antMatchers(ADMIN_MATCHER).hasRole("ADMIN")
-//		.antMatchers(CUSTOMER_MATCHER).hasRole("CUSTOMER")
-////		.antMatchers("/**/**").permitAll()
-//		.and()
-//		.formLogin();
-//	}
-//	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance();};
-//	
-//	private final static String[] ADMIN_MATCHER = {
-//	"/utenti/**/**",
-//	
-//	"/mezzi/**/**",
-//	
-//	"/prenotazioni/**/**",
-//};
-//
-//private final static String[] CUSTOMER_MATCHER = {
-//	"/utenti/controlla/**",
-//	"/utenti/id/**",
-//	"/utenti/singolo/**",
-//	"/utenti/modificaProfilo/**",
-//	
-//	"/mezzi/catalogo/**",
-//	
-//	"/prenotazioni/"
-//};
-	
-//	@Bean
-//	public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance();};
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
